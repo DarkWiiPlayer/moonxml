6d17c686ef1d89cb29305c53e3f6f37bf3aaf439
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2019-01-04 18:49:59 +0100

    Fix `hack` not passing arguments

--- xml.lua
+++ xml.lua
@@ -78,15 +78,15 @@ do
   end
   local hack
   if lua51 then
-    hack = function(self, fnc)
+    hack = function(self, fnc, ...)
       if not type(fnc) == 'function' then
         error('wrong argument, expecting function, got ' .. type(fnc), 2)
       end
       setfenv(fnc, self.environment)
-      return fnc()
+      return fnc(...)
     end
   else
-    hack = function(self, fnc)
+    hack = function(self, fnc, ...)
       assert(type(fnc) == 'function', 'wrong argument, expecting function, got ' .. type(fnc))
       do
         local upvaluejoin = debug.upvaluejoin
@@ -95,7 +95,7 @@ do
           return aaaa()
         end), 1)
       end
-      return fnc()
+      return fnc(...)
     end
   end
   local loadmoon
